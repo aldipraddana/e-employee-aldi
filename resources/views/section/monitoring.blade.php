a0032cae7dd60d67b97b17f237dff5e3912416d5
aldipraddana <aldipraddana@gmail.com>
2024-06-14 15:11:04 +0700

    feat: fix employee table

--- resources/views/section/monitoring.blade.php
+++ resources/views/section/monitoring.blade.php
@@ -15,19 +15,19 @@
             <div class="col-md-12">
                 <div class="table-responsive">
                     <table class="table table-striped text-center employee-table--datatable">
-                        <thead class="">
+                        <thead>
                             <tr>
                                 <th scope="col">No</th>
-                                <th scope="col">Nama</th>
-                                <th scope="col">Tempat Lahir</th>
+                                <th scope="col" style="min-width: 180px">Nama</th>
+                                <th scope="col" style="min-width: 150px">Tempat Lahir</th>
+                                <th scope="col" style="min-width: 150px">Tanggal Lahir</th>
                                 <th scope="col">Email</th>
-                                <th scope="col">Phone Number</th>
+                                <th scope="col" style="min-width: 150px">Phone Number</th>
                                 <th scope="col">Dokumen</th>
-                                <th scope="col">Tanggal Masuk</th>
-                                <th scope="col">Tanggal Keluar</th>
-                                <th scope="col">Tanggal Keluar</th>
+                                <th scope="col" style="min-width: 150px">Tanggal Masuk</th>
+                                <th scope="col" style="min-width: 150px">Tanggal Keluar</th>
                                 <th scope="col">Pekerjaan</th>
-                                <th scope="col">Tempat Kerja</th>
+                                <th scope="col" style="min-width: 130px">Tempat Kerja</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -39,11 +39,11 @@
                                     <td>{{ $item->date_of_birth }}</td>
                                     <td>{{ $item->email }}</td>
                                     <td>{{ $item->phone }}</td>
-                                    <td>{{ $item->name }}</td>
-                                    <td>{{ $item->name }}</td>
-                                    <td>{{ $item->name }}</td>
-                                    <td>{{ $item->name }}</td>
-                                    <td>{{ $item->name }}</td>
+                                    <td>-</td>
+                                    <td>{{ $item->work_entry_date }}</td>
+                                    <td>{{ $item->work_end_date }}</td>
+                                    <td>{{ $item->job }}</td>
+                                    <td>{{ $item->workplace }}</td>
                                 </tr>
                             @endforeach
                         </tbody>
